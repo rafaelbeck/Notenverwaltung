713cd6b4a695435c5b3fbf86438910650dab268c
Rafael Beck <hello@rafaelbeck.com>
2015-05-10 22:55:23 +0200

    Assoziation zwischen Fach und Pruefung

--- src/pd/Fach.java
+++ src/pd/Fach.java
@@ -1,14 +1,18 @@
 package pd;
 
+import java.util.ArrayList;
+
 public class Fach {
 	private String bezeichnung;
 	private int gewicht;
+	private ArrayList<Pruefung> pruefungen;
 	public Fach(String bezeichnung, int gewicht) {
 		if(!pruefeGewicht(gewicht)) {
 			throw new IllegalArgumentException("Falsches Gewicht");
 		}
 		this.bezeichnung = bezeichnung;
 		this.gewicht = gewicht;
+		this.pruefungen = new ArrayList<Pruefung>();
 	}
 	public String getBezeichnung() {
 		return bezeichnung;
@@ -19,8 +23,49 @@ public class Fach {
 	public int getGewicht() {
 		return gewicht;
 	}
+	//Lösung mit Java 7
+	//public double getFachnote() {
+	// 	double notenSumme = 0.0;
+	// 	int notenAnzahl = 0;
+	//	for(Pruefung p : pruefungen) {
+	//		if (p.getNoteVorhanden()) {
+	//			notenSumme += p.getNote();
+	//			notenAnzahl += 1;
+	//		}
+	//	}
+	//	return (notenAnzahl==0) ? 0.0:(notenSumme/notenAnzahl);
+	//}
+	public double getFachnote() {
+		return pruefungen
+				.stream()
+				.filter(Pruefung::getNoteVorhanden)
+				.mapToDouble(Pruefung::getNote)
+	            .average().orElse(0.0);
+	}
+	public ArrayList<Pruefung> getPruefungen() {
+		return pruefungen;
+	}
+	public Pruefung getPruefung(String datum) {
+		for(Pruefung p : pruefungen) {
+			if (p.getDatum().equals(datum)) {
+				return p;
+			}
+		}
+		return null;
+	}
+	public void neuePruefungHinzufügen(String datum) {
+		this.pruefungen.add(new Pruefung(datum));
+	}
+	public void neuePruefungHinzufügen(String datum, double note) {
+		this.pruefungen.add(new Pruefung(datum, note));
+	}
+	public int getAnzahlPruefungen() {
+		return pruefungen.size();
+	}
 	public String toString() {
 		return "Fach:   " + getBezeichnung() +
-				"\n        Gewicht: " + getGewicht();
+				"\n        Gewicht: " + getGewicht() +
+				"\n        Pruefungen:" + getPruefungen() +
+				"\n        Fachnote:" + getFachnote();
 	}
 }
--- src/tester/FachTester.java
+++ src/tester/FachTester.java
@@ -1,13 +1,20 @@
 package tester;
 
 import pd.Fach;
+import pd.Pruefung;
 
 public class FachTester {
 
 	public static void main(String[] args) {
 		try {
 			Fach f = new Fach("Mathe", 1);
-			System.out.println(f);
+			f.neuePruefungHinzufügen("01.01.2000", 5);
+			f.neuePruefungHinzufügen("15.01.2000");
+			Pruefung p = f.getPruefung("18.02.2000");
+			if (p!=null) {
+				p.setNote(5.8);
+			}
+			System.out.println(p);
 		} catch ( IllegalArgumentException e) {
 			System.out.println("Falsches Argument:" + e);
 		}
