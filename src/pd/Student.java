476437ec86cbd10541466f2276abfee10cce5f44
Rafael Beck <hello@rafaelbeck.com>
2015-05-17 16:31:30 +0200

    Assoziation zwischen Student und Fach

--- src/pd/Student.java
+++ src/pd/Student.java
@@ -1,13 +1,16 @@
 package pd;
 
+import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Student {
 	private String name;
 	private String vorname;
 	private Date gebDatum;
+	private ArrayList<Fach> faecher;
 	public Student(String vorname, String name, String gebDatum) {
 		this.name = name;
 		this.vorname = vorname;
@@ -17,7 +20,12 @@ public class Student {
 		} catch (ParseException pe) {
 			throw new IllegalArgumentException("Falsches Datum");
         }
+		this.faecher = new ArrayList<Fach>();
 	}
+	public void neuesFachHinzufügen(String bezeichnung, int gewicht) {
+		this.faecher.add(new Fach(bezeichnung, gewicht));
+	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -37,8 +45,31 @@ public class Student {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 		return dateFormat.format(gebDatum);
 	}
+	public int getAnzahlFaecher() {
+		return faecher.size();
+	}
+	public Fach getFach(String bezeichnung) {
+		for(Fach f : faecher) {
+			if (f.getBezeichnung().equals(bezeichnung)) {
+				return f;
+			}
+		}
+		return null;
+	}
+	public double getNotendurchschnitt() {
+		double notenSumme = 0;
+		double gewichtSumme = 0;
+		int faecherAnzahl = 0;
+		for(Fach f : faecher) {
+			notenSumme += (f.getGewicht() * f.getFachnote());
+			gewichtSumme += f.getGewicht();
+			faecherAnzahl++;
+		}
+		return (faecherAnzahl == 0) ? 0.0:notenSumme/gewichtSumme;
+	}
 	public String toString() {
 		return "Stundent:   " + getVollerName() +
-				"\n            Geburtsdatum: " + getGebDatum();
+				"\n            Geburtsdatum: " + getGebDatum() +
+				"\n            Anzahl Fächer: " + getAnzahlFaecher();
 	}
 }
--- src/tests/StudentTest.java
+++ src/tests/StudentTest.java
@@ -3,6 +3,9 @@ package tests;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import pd.Fach;
 import pd.Student;
 
 public class StudentTest {
@@ -18,5 +21,40 @@ public class StudentTest {
 	public void testGanzerNameAendern() {
 		s.setVorname("Franz Karl");
 		s.setName("Wissbegierig Vergesslich");
+	}
+	
+	@Test
+	public void testOhneFach() {
+		assertEquals(0, s.getAnzahlFaecher());
+	}
+	
+	@Test
+	public void testMitEinemFach() {
+		s.neuesFachHinzufügen("Psychologie", 1);
+		assertEquals(1, s.getAnzahlFaecher());
+	}
+	
+	@Test
+	public void testSucheFach() {
+		s.neuesFachHinzufügen("Psychologie", 1);
+		Fach f = s.getFach("Psychologie");
+		assertThat(f, instanceOf(Fach.class));
+	}
+	
+	@Test
+	public void testNotendurchschnitt() {
+		s.neuesFachHinzufügen("Psychologie", 1);
+		s.neuesFachHinzufügen("Informatik", 2);
+		s.neuesFachHinzufügen("UCD", 1);
+		
+		Fach f1 = s.getFach("Psychologie");
+		Fach f2 = s.getFach("Informatik");
+		Fach f3 = s.getFach("UCD");
+		
+		f1.neuePruefungHinzufügen("01.05.2015", 5);
+		f2.neuePruefungHinzufügen("02.05.2015", 6);
+		f3.neuePruefungHinzufügen("03.05.2015", 5.5);
+		
+		assertEquals(5.625, s.getNotendurchschnitt(), 0.001);
 	}
 }
